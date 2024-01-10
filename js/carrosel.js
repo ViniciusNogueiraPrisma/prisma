811f76318a845d51ca237cca92bb306e8bc64d18
vinicius nogueira <v.francisco@riprisma.com>
2024-01-10 18:41:48 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,36 +1,12 @@
-var links = document.getElementsByTagName("a");
-
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function (event) {
-    // Impede o comportamento padrão do link
-    event.preventDefault();
-  });
-}
-
-// var swiperLideres = null;
-
-// function initSwiper() {
-//   if (window.innerWidth < 769) {
-//     if (swiperLideres === null) {
-//       swiperLideres = new Swiper(".mySwiperLider", {
-//         slidesPerView: "auto",
-//         spaceBetween: 24,
-//         autoplay: {
-//           delay: 5000,
-//           disableOnInteraction: false,
-//         },
-//         pagination: {
-//           el: ".swiper-pagination",
-//         },
-//       });
-//     }
-//   } else {
-//     if (swiperLideres !== null) {
-//       swiperLideres.destroy(true, true);
-//       swiperLideres = null;
-//     }
-//   }
-// }
-
-// initSwiper();
-// window.addEventListener("resize", initSwiper);
+var swiper = new Swiper(".mySwiper", {
+  loop: false, // Desativa o loop
+  slidesPerView: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    // type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
