754066bda2a5b6712ce32094e72392fd61fdcd98
vinicius nogueira <v.francisco@riprisma.com>
2024-01-31 13:13:45 -0300

    alt

--- js/main.js
+++ js/main.js
@@ -236,6 +236,28 @@ $("#contrast-toggle").on("click", function (e) {
 
 /* Hover do menu */
 
+const counterUp = window.counterUp.default;
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    const el = entry.target;
+    if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+      counterUp(el, {
+        duration: 2000,
+        // delay: 16,
+      });
+      el.classList.add("is-visible");
+    }
+  });
+};
+
+const IO = new IntersectionObserver(callback, { threshold: 1 });
+const elements = document.querySelectorAll(".counter");
+
+elements.forEach((el) => {
+  IO.observe(el);
+});
+
 $(
   ".header .nav-menu-header .header-navigation-item > .dropdown-toggle"
 ).removeAttr("data-bs-toggle");
